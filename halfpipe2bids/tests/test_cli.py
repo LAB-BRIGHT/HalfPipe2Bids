2fc08681a4a749a286b6a9e608b35ec7d86c3f51
Hao-Ting Wang <htwangtw@gmail.com>
2025-06-18 11:49:55 -0400

    make test more explicit and edit notes

--- halfpipe2bids/tests/test_cli.py
+++ halfpipe2bids/tests/test_cli.py
@@ -55,10 +55,9 @@ def test_smoke(tmp_path, caplog):
     )
     assert relmat_file.exists()
     relmat = pd.read_csv(relmat_file, sep="\t")
-    # This is the number of ROI I got from the supossingly original file
-    assert (
-        len(relmat) == 434
-    )  # TODO: when the --impute-nans option is added, this might
+    # This is the number of ROI (columns) I got from the supposedly original file
+    assert relmat.shape[1] == 434
+    # TODO: when the --impute-nans option is added, this might
     json_file = output_folder / (ts_base + "_timeseries.json")
     assert json_file.exists()
     with open(json_file, "r") as f:
